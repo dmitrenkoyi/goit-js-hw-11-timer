2183af80547019ad419a71d9e5d035238c42ad08
dmitrenkoyi <artemovayi@gmail.com>
2021-06-04 17:37:40 +0300

    fixed class in timerr.js

--- timer.js
+++ timer.js
@@ -1,17 +1,17 @@
-const refs = {
-  days: document.querySelector('[data-value="days"]'),
-  hours: document.querySelector('[data-value="hours"]'),
-  mins: document.querySelector('[data-value="mins"]'),
-  secs: document.querySelector('[data-value="secs"]'),
-}
-
 class CountdownTimer{
      constructor({ targetDate, onTick }) {
          this.intervalId = null;
          this.targetDate = targetDate;
          this.onTick = onTick;
          this.start();
-    }
+       
+       this.refs = {
+         days: document.querySelector('[data-value="days"]'),
+         hours: document.querySelector('[data-value="hours"]'),
+         mins: document.querySelector('[data-value="mins"]'),
+         secs: document.querySelector('[data-value="secs"]'),
+       };
+  } 
 
      start() {
     const startTime = this.targetDate;
@@ -26,7 +26,7 @@ class CountdownTimer{
 
       const time = this.getTimeComponents(deltaTime);
 
-      this.onTick(time);
+      this.updateClockFace(time);
     }, 1000);
     }
     
@@ -43,17 +43,18 @@ getTimeComponents(time) {
 
     return { days, hours, mins, secs };
     }
+
+updateClockFace({ days, hours, mins, secs }) {
+  this.refs.days.textContent = `${days}`;
+  this.refs.hours.textContent = `${hours}`;
+  this.refs.mins.textContent = `${mins}`;
+  this.refs.secs.textContent = `${secs}`;
+  };
 }
 
 new CountdownTimer({
-    onTick: updateClockFace,
+    onTick:  '#timer-1',
     targetDate: new Date('Jan 01, 2022'),
 });
 
 
-function  updateClockFace({ days, hours, mins, secs }) {
-refs.days.textContent = `${days}`;
-refs.hours.textContent = `${hours}`;
-refs.mins.textContent = `${mins}`;
-refs.secs.textContent = `${secs}`;
-};
